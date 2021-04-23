e4222e099d7846ead8814ee3d1c109e8958c1452
Raul Moscoso <raul.moscoso84@gmail.com>
2021-04-23 14:55:40 -0500

    Redefinir metodo save de FORM de Django

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -75,9 +75,16 @@ class FormularioUsuario(forms.ModelForm):
 		Excepcoiones:
 		- ValidationError -- cuando las contrasenas no son iguales muetra un mensaje de error
 		"""	
-
-		password1 = self.cleaned_data.get['password1']
-		password2 = self.cleaned_data.get['password2']
+		print(self.cleaned_data)
+		password1 = self.cleaned_data.get('password1')
+		password2 = self.cleaned_data.get('password2')
 		if password1 and password2 and password1 != password2:
 			raise forms.ValidationError('Contrasenas no coinciden!')
 		return password2
+
+	def save(self, commit = True):
+		user = super().save(commit = False)
+		user.set_password(self.cleaned_data['password1'])
+		if commit:
+			user.save()
+		return user
